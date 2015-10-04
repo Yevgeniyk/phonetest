fa994d6a3bf477173e4fe9ad2dd11b4366d40a1e
ykelman <yevgeniy.kelman@allstate.com>
2015-10-04 13:44:04 -0500

    Initial Commit

---
+++ iPhoneTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iPhoneTest
+//
+//  Created by Kelman, Yevgeniy on 10/4/15.
+//  Copyright (c) 2015 Kelman, Yevgeniy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iPhoneTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iPhoneTest
+//
+//  Created by Kelman, Yevgeniy on 10/4/15.
+//  Copyright (c) 2015 Kelman, Yevgeniy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
